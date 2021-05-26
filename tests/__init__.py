60dbf3444cbd9b2b3291498a97b4500f39ea3460
Johan Isacsson <johan@generatorhallen.se>
2021-05-26 08:40:50 +0200

    First commit

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for SunSpec integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for SunSpec tests."""
+from custom_components.sunspec.const import (
+    CONF_PASSWORD,
+)
+from custom_components.sunspec.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
