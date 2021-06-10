39ac57d431783be68a308051e2726f83e3b79736
Johan Isacsson <johan@generatorhallen.se>
2021-06-10 11:26:01 +0200

    Version 0.0.3

--- custom_components/sunspec/const.py
+++ custom_components/sunspec/const.py
@@ -3,7 +3,7 @@
 NAME = "SunSpec"
 DOMAIN = "sunspec"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 ATTRIBUTION = "Data provided by SunSpec alliance - https://sunspec.org"
 ISSUE_URL = "https://github.com/cjne/ha-sunspec/issues"
