7e3ad6fa396e4f8018adfebfacfe7aced7515908
Johan Isacsson <johan@generatorhallen.se>
2021-06-10 11:25:09 +0200

    Proper icons and device classes

--- custom_components/sunspec/sensor.py
+++ custom_components/sunspec/sensor.py
@@ -4,6 +4,11 @@ import logging
 from homeassistant.const import DATA_RATE_BITS_PER_SECOND
 from homeassistant.const import DATA_RATE_MEGABITS_PER_SECOND
 from homeassistant.const import DEGREE
+from homeassistant.const import DEVICE_CLASS_CURRENT
+from homeassistant.const import DEVICE_CLASS_ENERGY
+from homeassistant.const import DEVICE_CLASS_POWER
+from homeassistant.const import DEVICE_CLASS_TEMPERATURE
+from homeassistant.const import DEVICE_CLASS_VOLTAGE
 from homeassistant.const import ELECTRICAL_CURRENT_AMPERE
 from homeassistant.const import ELECTRICAL_VOLT_AMPERE
 from homeassistant.const import ENERGY_KILO_WATT_HOUR
@@ -26,37 +31,38 @@ from .entity import SunSpecEntity
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
-HA_UNITS = {
-    "A": ELECTRICAL_CURRENT_AMPERE,
-    "HPa": PRESSURE_HPA,
-    "Hz": FREQUENCY_HERTZ,
-    "Mbps": DATA_RATE_MEGABITS_PER_SECOND,
-    "V": VOLT,
-    "VA": ELECTRICAL_VOLT_AMPERE,
-    "W": POWER_WATT,
-    "W/m2": IRRADIATION_WATTS_PER_SQUARE_METER,
-    "Wh": ENERGY_WATT_HOUR,
-    "WH": ENERGY_WATT_HOUR,
-    "bps": DATA_RATE_BITS_PER_SECOND,
-    "deg": DEGREE,
-    "Degrees": DEGREE,
-    "C": TEMP_CELSIUS,
-    "kWh": ENERGY_KILO_WATT_HOUR,
-    "m/s": SPEED_METERS_PER_SECOND,
-    "mSecs": TIME_MILLISECONDS,
-    "meters": LENGTH_METERS,
-    "mm": LENGTH_MILLIMETERS,
-    "%": PERCENTAGE,
-    "Secs": TIME_SECONDS,
-}
-
-ICON_DEFAULT = "mdi:solar-panel"
-ICON_AMPS = "mdi:solar-panel"
-ICON_VOLT = "mdi:solar-panel"
-ICON_POWER = "mdi:solar-panel"
+ICON_DEFAULT = "mdi:information-outline"
+ICON_AC_AMPS = "mdi:current-ac"
+ICON_DC_AMPS = "mdi:current-dc"
+ICON_VOLT = "mdi:lightning-bolt"
+ICON_POWER = "mdi:solar-power"
 ICON_FREQ = "mdi:solar-panel"
 ICON_ENERGY = "mdi:solar-panel"
-ICON_TEMP = "mdi:temperature"
+ICON_TEMP = "mdi:thermometer"
+
+HA_META = {
+    "A": [ELECTRICAL_CURRENT_AMPERE, ICON_AC_AMPS, DEVICE_CLASS_CURRENT],
+    "HPa": [PRESSURE_HPA, ICON_DEFAULT, DEVICE_CLASS_TEMPERATURE],
+    "Hz": [FREQUENCY_HERTZ, ICON_FREQ, None],
+    "Mbps": [DATA_RATE_MEGABITS_PER_SECOND, ICON_DEFAULT, None],
+    "V": [VOLT, ICON_VOLT, DEVICE_CLASS_VOLTAGE],
+    "VA": [ELECTRICAL_VOLT_AMPERE, ICON_POWER, DEVICE_CLASS_POWER],
+    "W": [POWER_WATT, ICON_POWER, DEVICE_CLASS_POWER],
+    "W/m2": [IRRADIATION_WATTS_PER_SQUARE_METER, ICON_DEFAULT, None],
+    "Wh": [ENERGY_WATT_HOUR, ICON_ENERGY, DEVICE_CLASS_ENERGY],
+    "WH": [ENERGY_WATT_HOUR, ICON_ENERGY, DEVICE_CLASS_ENERGY],
+    "bps": [DATA_RATE_BITS_PER_SECOND, ICON_DEFAULT, None],
+    "deg": [DEGREE, ICON_TEMP, DEVICE_CLASS_TEMPERATURE],
+    "Degrees": [DEGREE, ICON_TEMP, DEVICE_CLASS_TEMPERATURE],
+    "C": [TEMP_CELSIUS, ICON_TEMP, DEVICE_CLASS_TEMPERATURE],
+    "kWh": [ENERGY_KILO_WATT_HOUR, ICON_ENERGY, DEVICE_CLASS_ENERGY],
+    "m/s": [SPEED_METERS_PER_SECOND, ICON_DEFAULT, None],
+    "mSecs": [TIME_MILLISECONDS, ICON_DEFAULT, None],
+    "meters": [LENGTH_METERS, ICON_DEFAULT, None],
+    "mm": [LENGTH_MILLIMETERS, ICON_DEFAULT, None],
+    "%": [PERCENTAGE, ICON_DEFAULT, None],
+    "Secs": [TIME_SECONDS, ICON_DEFAULT, None],
+}
 
 
 async def async_setup_entry(hass, entry, async_add_devices):
@@ -85,7 +91,6 @@ class SunSpecSensor(SunSpecEntity):
 
     def __init__(self, coordinator, config_entry, data):
         super().__init__(coordinator, config_entry, data["device_info"])
-        self.use_icon = ICON_DEFAULT
         self.model_id = data["model_id"]
         self.model_index = data["model_index"]
         self.model_wrapper = data["model"]
@@ -93,11 +98,14 @@ class SunSpecSensor(SunSpecEntity):
         self._meta = self.model_wrapper.getMeta(self.key)
         self._group_meta = self.model_wrapper.getGroupMeta()
         sunspec_unit = self._meta.get("units", "")
-        self.unit = HA_UNITS.get(sunspec_unit, sunspec_unit)
+        ha_meta = HA_META.get(sunspec_unit, [sunspec_unit, ICON_DEFAULT, None])
+        self.unit = ha_meta[0]
+        self.use_icon = ha_meta[1]
+        self.use_device_class = ha_meta[2]
+
         self._uniqe_id = (
             f"{config_entry.entry_id}_{self.key}-{self.model_id}-{self.model_index}"
         )
-        # _LOGGER.debug(f"Init sensor {self._uniqe_id}")
         self._device_id = config_entry.entry_id
         name = self._group_meta.get("name", str(self.model_id))
         if self.model_index > 0:
@@ -106,7 +114,11 @@ class SunSpecSensor(SunSpecEntity):
         if len(key_parts) > 1:
             name = f"{name} {key_parts[0]} {key_parts[1]}"
 
-        self._name = f"{name.capitalize()} {self._meta.get('desc', self._meta.get('label', self.key))}"
+        desc = self._meta.get("desc", self._meta.get("label", self.key))
+        if self.unit == ELECTRICAL_CURRENT_AMPERE and "DC" in desc:
+            self.use_icon = ICON_DC_AMPS
+
+        self._name = f"{name.capitalize()} {desc}"
 
     # def async_will_remove_from_hass(self):
     #    _LOGGER.debug(f"Will remove sensor {self._uniqe_id}")
@@ -156,7 +168,7 @@ class SunSpecSensor(SunSpecEntity):
     @property
     def device_class(self):
         """Return de device class of the sensor."""
-        return "sunspec__custom_device_class"
+        return self.use_device_class
 
     @property
     def device_state_attributes(self):
